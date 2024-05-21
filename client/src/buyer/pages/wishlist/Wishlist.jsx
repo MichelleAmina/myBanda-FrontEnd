e074d2c359ee36841f6dbdfd08f293db31b7f3ff
Michael <michaelmwendwamumo@gmail.com>
2024-05-21 16:17:32 +0300

    Rendered products by passing it down to homeproduct

--- client/src/buyer/pages/wishlist/Wishlist.jsx
+++ client/src/buyer/pages/wishlist/Wishlist.jsx
@@ -5,30 +5,50 @@ import HomeProduct from '../../components/product/HomeProduct';
 function Wishlist(){
     // store product data
     const [productData, setProductData] = useState([]);
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
         fetch("https://mybanda-backend-88l2.onrender.com/like")
             .then(resp => resp.json())
             .then((data) => {
                 setProductData(data)
-                console.log(data);;
+                setLoading(false)
+                console.log("wishlist data",data);
             })
             .catch(error => {
                 console.error('Error fetching liked products:', error);
+                setError(error)
+                setLoading(falase)
             });
     }, []);
+
+    //console.log("wishlist", productData[0].product)
+
+    if (loading){
+        return <div>Loading...</div>
+    }
+
+    if (error){
+        return <div>Error loading likes</div>
+    }
+
+
 
     return(
         <div className="wishlist">
             <div className="container-fluid">
                 Wishlist
-                <div className="productRow ps-4">
-                    {productData.length !== 0 ? productData.map((item, index) => (
-                        <div className="item" key={index}>
-                            <HomeProduct item={item}/> 
-                        </div>
-                    )):
-                    <p>Loading...</p>}
+                <div className="wishlistProducts ps-4">
+                    {productData.length !== 0 &&
+                        // <h2>{productData[0].product.name}</h2>
+                        productData.map((item, index) => (
+                            <div className="item" key={index}>
+                                {console.log("Item passed down to home product:",item.product)}
+                                <HomeProduct item={item.product}/>
+                            </div>
+                        ))
+                    }           
                 </div>
             </div>
         </div>
