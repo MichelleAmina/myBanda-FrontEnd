59baec5280992afcdbdb5c68cceddb6295e1dc2b
Michael <michaelmwendwamumo@gmail.com>
2024-05-22 21:19:31 +0300

    Styled add to cart

--- client/src/buyer/pages/wishlist/Wishlist.jsx
+++ client/src/buyer/pages/wishlist/Wishlist.jsx
@@ -1,12 +1,22 @@
 import './wishlist.css'
 import React, { useState, useEffect } from 'react';
-import HomeProduct from '../../components/product/HomeProduct';
+// import HomeProduct from '../../components/product/HomeProduct';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../../../redux/cartSlice';
+import Rating from '@mui/material/Rating';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 function Wishlist(){
     // store product data
     const [productData, setProductData] = useState([]);
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(null)
+    const dispatch = useDispatch();
+
+    const handleAddToCart = (item) => {
+        dispatch(addToCart(item)); 
+    };
+
 
     useEffect(() => {
         fetch("https://mybanda-backend-88l2.onrender.com/like")
@@ -33,8 +43,6 @@ function Wishlist(){
         return <div>Error loading likes</div>
     }
 
-
-
     return(
         <div className="wishlist">
             <div className="container-fluid">
@@ -57,17 +65,21 @@ function Wishlist(){
                                 <li key={item.id} className="wishlist-item">
                                     <img src={item.product.images[0].image_url} alt={item.name} className="wishlist-item-image" />
                                     <div className="wishlist-item-details">
-                                        <h2>{item.name}</h2>
+                                        <h2>{item.product.name.toUpperCase()}</h2>
+                                        <h2>{item.product.description}</h2>
+                                        <Rating name="half-rating-read" defaultValue={3.5} precision={0.5} readOnly/>
                                         <div className="wishlist-item-price">
                                             <span>{item.product.price}</span>
                                         </div>
+                                        <div className='wishlist-item-description'>
+
+                                        </div>
                                     </div>
-                                    <button className="wishlist-item-button">Add To Cart</button>
+                                    <button className="wishlist-item-button" onClick={()=> handleAddToCart(item.product)}><ShoppingCartOutlinedIcon />Add To Cart</button>
                                 </li>
                             ))}
                         </ul>
                     </div>
-                    {/* <h1>{productData[0].product.name}</h1> */}
                 </div>
             </div>
         </div>
