c47d85522fbbd2306a671c6308aa529e91b50095
Michelle Amina Okumu <michelleaokumu@gmail.com>
2024-05-24 12:45:24 +0300

    Commit changes to wishlist

--- client/src/buyer/pages/wishlist/Wishlist.jsx
+++ client/src/buyer/pages/wishlist/Wishlist.jsx
@@ -41,7 +41,7 @@ function Wishlist(){
             .then((data) => {
                 setProductData(data)
                 setLoading(false)
-                console.log('Fetched wishlist:',data);
+                //console.log('Fetched wishlist:',data);
                 // console.log("wishlist data",data);
 
             })
@@ -54,8 +54,13 @@ function Wishlist(){
 
     
 
-    if (loading){
-        return <div>Loading...</div>
+    // Render loading GIF if loading
+    if (loading) {
+        return (
+            <div className="loader">
+                <img src="https://i.pinimg.com/originals/c1/bc/d8/c1bcd8a8c945b53da6b29f10a2a553c0.gif" alt="Loading..." />
+            </div>
+        );
     }
 
     if (error){
@@ -67,46 +72,53 @@ function Wishlist(){
     }
 
     return(
-        <div className="wishlist">
+        <div className="wishlistWrapper">
             <div className="container-fluid">
-                You have {productData.length} items on Wishlist
-                <div className="wishlistProducts ps-4">
-                    {/* {productData.length !== 0 &
-                        // <h2>{productData[0].product.name}</h2>
-                        productData.map((item, index) => (
-                            <div className="item" key={index}>
-                                {console.log("Item passed down to home product:",item.product)}
-                                <HomeProduct item={item.product}/>
-                            </div>
-                        ))
-                    }            */}
-                    <div className="wishlist-container">
-                        <ul className="wishlist-items">
-                            {
-                            productData.length !== 0 &&
-                            productData.map((item, index) => (
-                                <li key={item.id} className="wishlist-item">
-                                    <img src={item.product.images[0].image_url} alt={item.name} className="wishlist-item-image" />
-                                    <div className="wishlist-item-details">
-                                        <h2><NavLink to={`/my_banda/products/${item.product.id}`}>{item.product.name.toUpperCase()}</NavLink></h2>
-                                        <h2>{item.product.description}</h2>
-                                        <Rating name="half-rating-read" defaultValue={3.5} precision={0.5} readOnly/>
-                                        <div className="wishlist-item-price">
-                                            <span>{item.product.price}</span>
-                                        </div>
-                                        <div className='wishlist-item-description'>
-
-                                        </div>
-                                    </div>
-                                    <div className='wishlist-clicks'>
-                                        <button onClick={() => handleDeleteFromWishlist(item.id)}><DeleteIcon/></button>
-                                        <button className="wishlist-item-button" onClick={()=> handleAddToCart(item.product)}><ShoppingCartOutlinedIcon />Add To Cart</button>
-                                    </div>
-                                </li>
-                            ))}
-                        </ul>
+                <div className="wishlist">
+                    <div className="wishlistTitle">
+                        <h1>You have {productData.length} item/s on Wishlist</h1>
                     </div>
+
+                    <div className="wishlistProducts ps-4">
+                        {/* {productData.length !== 0 &
+                            // <h2>{productData[0].product.name}</h2>
+                            productData.map((item, index) => (
+                                <div className="item" key={index}>
+                                    {console.log("Item passed down to home product:",item.product)}
+                                    <HomeProduct item={item.product}/>
+                                </div>
+                            ))
+                        }            */}
+                        <div className="wishlist-container">
+                            <ul className="wishlist-items">
+                                {
+                                productData.length !== 0 &&
+                                productData.map((item, index) => (
+                                    <li key={item.id} className="wishlist-item">
+                                        <img src={item.product.images[0].image_url} alt={item.name} className="wishlist-item-image" />
+                                        <div className="wishlist-item-details">
+                                            <NavLink to={`/my_banda/products/${item.product.id}`} style={{textDecoration:"none", color:"#000"}}><h2>{item.product.name}</h2></NavLink>
+                                            <h4>{item.product.description}</h4>
+                                            <Rating name="half-rating-read" defaultValue={3.5} precision={0.5} readOnly/>
+                                            <div className="wishlist-item-price">
+                                                <span>$ {item.product.price}</span>
+                                            </div>
+                                            <div className='wishlist-item-description'>
+
+                                            </div>
+                                        </div>
+                                        <div className='wishlist-clicks'>
+                                            <button onClick={() => handleDeleteFromWishlist(item.id)} className='delete-button'><DeleteIcon/></button>
+                                            <button className="wishlist-item-button" onClick={()=> handleAddToCart(item.product)}><ShoppingCartOutlinedIcon />Add To Cart</button>
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+
                 </div>
+                            
             </div>
         </div>
     )
