b3e6a55d67c7a233e7cdc6910c68cd0f175b527a
Michelle Amina Okumu <michelleaokumu@gmail.com>
2024-05-25 15:19:01 +0300

    changes to my_banda: TopProducts, Banners, Homeproduct

--- client/src/buyer/pages/home/topProducts/TopProducts.jsx
+++ client/src/buyer/pages/home/topProducts/TopProducts.jsx
@@ -1,79 +1,10 @@
-// import './topProducts.css'
-// import { NavLink } from 'react-router-dom'
-// import Rating from '@mui/material/Rating';
-
-
-// function TopProducts(title){
-//     return(
-//         <>
-//         <div className="topSellingBox">
-//             <h3>{title.title}</h3>
-
-//             <div className="items d-flex align-items-center">
-//                 <div className="img">
-//                     {/*wrap image in navlink?*/}
-//                     <img src="./phone.png" alt="" className='w-100'/>
-
-//                 </div>
-
-//                 <div className="info px-3">
-//                     <NavLink to=""><h4>Lorem ipsum dolor sit amet amet</h4></NavLink>
-//                     <Rating name="half-rating-read" defaultValue={3.5} precision={0.5} readOnly />
-//                     <div className="d-flex align-items-center">
-//                         <span className='price'>$80</span><span className='oldPrice'>$150</span>
-//                     </div>
-
-//                 </div>
-//             </div>
-
-//             <div className="items d-flex align-items-center">
-//                 <div className="img">
-//                     {/*wrap image in navlink?*/}
-//                     <img src="./phone.png" alt="" className='w-100'/>
-
-//                 </div>
-
-//                 <div className="info px-3">
-//                     <NavLink to=""><h4>Lorem ipsum dolor sit amet amet</h4></NavLink>
-//                     <Rating name="half-rating-read" defaultValue={3.5} precision={0.5} readOnly />
-//                     <div className="d-flex align-items-center">
-//                         <span className='price'>$80</span><span className='oldPrice'>$150</span>
-//                     </div>
-
-//                 </div>
-//             </div>
-
-//             <div className="items d-flex align-items-center">
-//                 <div className="img">
-//                     {/*wrap image in navlink?*/}
-//                     <img src="./phone.png" alt="" className='w-100'/>
-
-//                 </div>
-
-//                 <div className="info px-3">
-//                     <NavLink to=""><h4>Lorem ipsum dolor sit amet amet</h4></NavLink>
-//                     <Rating name="half-rating-read" defaultValue={3.5} precision={0.5} readOnly />
-//                     <div className="d-flex align-items-center">
-//                         <span className='price'>$80</span><span className='oldPrice'>$150</span>
-//                     </div>
-
-//                 </div>
-//             </div>
-//         </div>
-//         </>
-
-//     )
-// }
-
-// export default TopProducts
-
 import './topProducts.css'
 import { NavLink } from 'react-router-dom'
 import Rating from '@mui/material/Rating';
 
 function TopProducts({ title, tag, products }) {
-    // Filter the products based on the tag
-    const filteredProducts = products.filter(product => product.tag === tag);
+    // Filter the products based on the tag and get only the first 3 
+    const filteredProducts = products.filter(product => product.tag === tag).slice(0, 3);
 
     return (
         <div className="topSellingBox">
@@ -82,13 +13,13 @@ function TopProducts({ title, tag, products }) {
             {filteredProducts.map(product => (
                 <div className="items d-flex align-items-center" key={product.id}>
                     <div className="img">
-                        <NavLink to={`/product/${product.id}`}>
+                        <NavLink to={`/my_banda/products/${product.id}`}>
                             <img src={product.images[0].image_url} alt={product.name} className='w-100'/>
                         </NavLink>
                     </div>
                     <div className="info px-3">
-                        <NavLink to={`/product/${product.id}`}>
-                            <h4>{product.name}</h4>
+                        <NavLink to={`/my_banda/products/${product.id}`}>
+                            <h4>{product.description}</h4>
                         </NavLink>
                         <Rating name="half-rating-read" defaultValue={3.5} precision={0.5} readOnly />
                         <div className="d-flex align-items-center">
